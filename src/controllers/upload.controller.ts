c927bbd6ac591fb7f1e94f5ca5ebae7940e21031
alloboo <hye870425@gmail.com>
2024-01-19 17:37:10 +0900

    [add] controller 폴더 및 upload.controller.ts 추가

---
+++ src/controllers/upload.controller.ts
@@ -0,0 +1,25 @@
+import express, { NextFunction } from "express";
+import xlsx from "node-xlsx";
+
+export const uploadFile = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const file = req.body;
+    
+        if (!file) {
+            return res.status(400).json({ ok: false, msg: 'File not provided'})
+        }
+        // request로 전달된 '.xlsx' 파일을 'uploads' 폴더에 저장
+    
+        // 저장된 파일 읽기
+        // let workbook = xlsx.readfile('data.xlsx');
+    
+        // 읽은 파일의 sheet와 column 대로 db table 생성
+    
+        // 파일의 데이터를 db에 저장
+    
+        // db 저장이 완료되면 'uploads' 폴더에서 해당 파일 삭제
+
+    } catch (err) {
+        next(err);
+    }
+};
