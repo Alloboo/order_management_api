bf85c9582611730da34aba46d26da23cdf317e70
alloboo <hye870425@gmail.com>
2024-01-19 17:13:57 +0900

    [fix] DB_PORT 오류 해결

--- db.ts
+++ db.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const AppDataSource = new DataSource({
   type: "mysql",
   host: process.env.DB_HOST,
-  port: (process.env.DB_PORT, 10),
+  port: parseInt(process.env.DB_PORT as string, 10),
   username: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
